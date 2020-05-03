49f469b8a587f063f856a88ab3ad9194965c7bb4
Ivan <38044561+IvanMenshov@users.noreply.github.com>
2020-05-03 12:39:07 +0300

    Add project files.

---
+++ DiscussionPlatform/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DiscussionPlatform.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
