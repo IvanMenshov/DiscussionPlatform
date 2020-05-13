0f440c7968e396828b0fef2dbeae3e16189527c3
Ivan <38044561+IvanMenshov@users.noreply.github.com>
2020-05-13 12:24:30 +0300

    Profile View, fix login bug

--- DiscussionPlatform/Controllers/AccountController.cs
+++ DiscussionPlatform/Controllers/AccountController.cs
@@ -61,7 +61,7 @@ namespace DiscussionPlatform.Controllers
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
@@ -220,7 +220,7 @@ namespace DiscussionPlatform.Controllers
             ViewData["ReturnUrl"] = returnUrl;
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.UserName, Email = model.Email, PartnerSince = DateTime.Now};
                 var result = await _userManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
--- DiscussionPlatform/Models/AccountViewModels/RegisterViewModel.cs
+++ DiscussionPlatform/Models/AccountViewModels/RegisterViewModel.cs
@@ -23,5 +23,10 @@ namespace DiscussionPlatform.Models.AccountViewModels
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Username")]
+        [StringLength(20, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+        public string UserName { get; set; }
     }
 }
